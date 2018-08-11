6fcff68209dbde2c5a6be7519ed7323f8ae7241b
Szymon Oleszek <szymqwerty@gmail.com>
2018-08-11 15:04:03 +0200

    added readme and correct some jq stuff with smooth scrolling

---
+++ README.md
@@ -0,0 +1,23 @@
+# LoopLAB - one page site
+
+LoopLAB site is a one page training site which I made with Bootstap course for train my skills 
+
+# Resultat of work
+
+If you want watch progress and resultat of my work you can click on the link below or on the top of GH site 
+
+[LINK](to paste)
+
+## Tech
+
+> HTML 4
+> CSS 
+> Bootstrap
+> jQuery
+
+## Todos
+
+> validation of forms
+> change jq smooth scrolling to Vanilla JS smooth scrolling
+
+### Very thanks to Brad Traversy for his tutorial which constantly introduce to Front-end world
--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+// document.addEventListener('load', ()=>{
 
+// $('#year').text(new Date().getFullYear())
+
+// $('body').scrollspy({target: '#main-nav'})
+
+// const homeSectionBtn = document.querySelector('.home-link')
+// const exploreSectionBtn = document.querySelector('.explore-link')
+// const createSectionBtn = document.querySelector('.create-link')
+// const shareSectionBtn = document.querySelector('.share-link')
+
+// const homeSection = document.getElementById('home-section')
+// const exploreHeadSection = document.getElementById('explore-head-section')
+// const createeHeadSection = document.getElementById('create-head-section')
+// const shareHeadSection = document.getElementById('share-head-section')
+
+// console.log(shareHeadSectionBtn)
+
+// const scrollTo = (element) => {
+//     window.scroll({
+//         behavior: 'smooth',
+//         left: 0,
+//         top: element.offsetTop
+//     })
+// }
+// homeSectionBtn.addEventListener('click', ()=>{
+//     scrollTo(homeSection)
+// })
+// exploreSectionBtn.addEventListener('click', ()=>{
+//     scrollTo(exploreSection)
+// })
+// createSectionBtn.addEventListener('click', ()=>{
+//     scrollTo(createSection)
+// })
+// shareSectionBtn.addEventListener('click', ()=>{
+//     scrollTo(shareSection)
+// })
+// })
